135979df3cec927501f944e84b5b26b2129169d3
Yasir Shahzad <Yasirshahzad918@gmail.com>
2025-03-21 11:20:55 +0500

    Rename platformBegin to begin

--- src/FanControllerESP8266.h
+++ src/FanControllerESP8266.h
@@ -3,17 +3,17 @@
 
 #include "FanControllerBase.h"
 
-class FanControllerESP8266 : public FanControllerBase {
+class FanControllerESP8266 : public FanControllerBase  {
 public:
     FanControllerESP8266(int fanPin, int zeroCrossingPin)
-        : FanControllerBase(fanPin, zeroCrossingPin) {}
+        : FanControllerBase (fanPin, zeroCrossingPin) {}
 
 protected:
-    void platformBegin() override {
+    void begin() override {
         attachInterrupt(digitalPinToInterrupt(_zeroCrossingPin), fireTriac, RISING);
     }
 
-    void platformSetSpeed(int delayTime) override {
+    void setSpeed(int delayTime) override {
         // ESP8266-specific timer setup
         // Use timer1 for precise timing
     }
