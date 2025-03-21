f88914d2eca996ab9becb634fade22430a6ae8e3
Yasir Shahzad <Yasirshahzad918@gmail.com>
2025-03-21 11:20:08 +0500

    Add begin() and setSpeed() implementations to FanControllerBase

--- src/FanControllerBase.h
+++ src/FanControllerBase.h
@@ -4,20 +4,30 @@
 #include <Arduino.h>
 
 class FanControllerBase {
-public:
-    FanControllerBase(int fanPin, int zeroCrossingPin);
-    virtual ~FanControllerBase() = default;
-
-    void begin();
-    void setSpeed(int speed); // Speed in percentage (0-100)
-
 protected:
-    virtual void platformBegin() = 0;
-    virtual void platformSetSpeed(int delayTime) = 0;
-
     int _fanPin;
     int _zeroCrossingPin;
-    volatile int _delayTime;
+    int _delayTime;
+
+public:
+    FanControllerBase(int fanPin, int zeroCrossingPin)
+        : _fanPin(fanPin), _zeroCrossingPin(zeroCrossingPin), _delayTime(0) {}
+
+    virtual void begin() { // Make begin() virtual so it can be overridden
+        pinMode(_fanPin, OUTPUT);
+        pinMode(_zeroCrossingPin, INPUT);
+    }
+
+    void setSpeed(int speed) {
+        if (speed < 0) speed = 0;
+        if (speed > 100) speed = 100;
+        _delayTime = map(speed, 0, 100, 8000, 0); // Map speed to delay
+
+        platformSetSpeed(_delayTime); // Call platform-specific implementation
+    }
+
+protected:
+    virtual void platformSetSpeed(int delayTime) = 0; // Must be implemented in subclasses
 };
 
 #endif // FANCONTROLLERBASE_H
